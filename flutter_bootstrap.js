e3c5fa088545a8dd97f4ae444ac35629e402abc8
pz <p.aa.z@d50-92-230-69.bchsia.telus.net>
2026-05-31 20:41:42 -0700

    feat: Add 3-step animated app logo to OnboardingView

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1232198336" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2750396893" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
