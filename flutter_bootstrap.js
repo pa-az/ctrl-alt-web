d50353ed107948a4c78a34d4ce3df245059f1409
pz <p.aa.z@d50-92-230-69.bchsia.telus.net>
2026-06-02 05:48:06 -0700

    Deploy updated web section

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1276464819" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4156161029" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
