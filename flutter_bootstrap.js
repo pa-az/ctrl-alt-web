625c1ef205c90b7ee26a5643f8a546f8591ac71b
pz <p.aa.z@d50-92-230-69.bchsia.telus.net>
2026-06-03 18:04:33 -0700

    Update ShopModal to show unlocked premium cards

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "115643590" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1765091478" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
