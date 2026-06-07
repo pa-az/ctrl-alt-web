cee5c3c91a57b6c67b003c4b3a53b8af17124db1
pz <p.aa.z@d50-92-230-69.bchsia.telus.net>
2026-06-07 12:32:32 -0700

    Deploy AppBar fix

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "923832954" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2714480650" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
