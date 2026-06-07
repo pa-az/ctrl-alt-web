94c6edfb92c069ce104472f4545cbbef7a7c15f4
pz <p.aa.z@d50-92-230-69.bchsia.telus.net>
2026-06-06 18:52:42 -0700

    Deploy Pull-to-Refresh update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3248568414" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2762462937" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
