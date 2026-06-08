1715a0f130024e9470125a9bdc213554d289b495
pz <p.aa.z@paaz-MacBook-Pro.local>
2026-06-07 23:25:05 -0700

    Deploy premium theme tab crash fix

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "621243173" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3984482757" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
