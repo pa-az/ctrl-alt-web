ace3dbb78973378e4f93eece3136daaaec08d9a7
paazi <pozarpak@gmail.com>
2026-06-13 16:21:24 -0700

    deploy: random eye animations

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1109281795" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2371058270" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
