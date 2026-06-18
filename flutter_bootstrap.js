d912a2b5bbc5268baa1d84d875de2974b83a1b38
paazi <pozarpak@gmail.com>
2026-06-18 14:45:35 -0700

    Update eye logo to 10s blink with tired bags

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "332722934" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "613765284" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
