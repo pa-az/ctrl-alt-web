371f750cb6bd9a55ef93ca221c4f8cf6eaa79593
paazi <pozarpak@gmail.com>
2026-06-24 01:25:56 -0700

    Deploy webapp 1.1.5+21 with tutorial fixes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4092029585" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4274414792" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
