a972132a0a332488172fcac066e6538bb9a0e758
paazi <pozarpak@gmail.com>
2026-06-25 13:10:08 -0700

    Deploy webapp: fix textfield state issue in restore dialog

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3705765428" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3202821373" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
