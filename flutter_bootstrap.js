10deb801a32d6366f97740203c6273d5db8f77e2
paazi <pozarpak@gmail.com>
2026-06-26 14:21:14 -0700

    Deploy webapp: implement custom Instagram examples and animations for Matrix tab

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1251435835" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3775883029" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
