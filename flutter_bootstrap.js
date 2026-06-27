7e76b5e1d959228bd3706597878128565afa1a54
paazi <pozarpak@gmail.com>
2026-06-27 09:52:06 -0700

    Deploy webapp: rewrite AI-style dash explanations to natural phrasing

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3203638753" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1299755101" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
