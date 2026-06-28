b71406cdebeba8def9dada374729aa052977c811
paazi <pozarpak@gmail.com>
2026-06-28 12:51:04 -0700

    Deploy webapp: move Level stat into progress bar

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "70735649" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2484079172" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
