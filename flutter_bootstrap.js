18efb232780958b265672c7731630c49d3a2b82b
paazi <pozarpak@gmail.com>
2026-06-28 13:09:33 -0700

    Deploy webapp: move XP stat to center of progress bar

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2484079172" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4006203138" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
