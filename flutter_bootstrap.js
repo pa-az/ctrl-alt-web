741a714bd40d8420774b5493096f992186a70b1a
paazi <pozarpak@gmail.com>
2026-06-28 15:21:19 -0700

    Deploy webapp: fix missing extensions for flutter_animate

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2324298472" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3057707239" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
