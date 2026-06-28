1e24cef3432f14a75e9ebd97ebd8a8a9723df35c
paazi <pozarpak@gmail.com>
2026-06-28 15:53:19 -0700

    Deploy webapp: remove black background from brain image

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "249131647" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2210503507" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
