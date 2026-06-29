79322e9ccc6eda465c1a3b09f2490f64ad989679
paazi <pozarpak@gmail.com>
2026-06-28 18:31:10 -0700

    Deploy webapp: animated single toggle button for view

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2474371392" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1056249807" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
