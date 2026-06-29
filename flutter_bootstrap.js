94466cedf61a1eba8e4146907a37386819ab85a7
paazi <pozarpak@gmail.com>
2026-06-29 08:18:01 -0700

    Deploy webapp: Refactor card back layout to inline sections (Data, Fix, Origin) without separate container

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2924660184" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3763798995" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
