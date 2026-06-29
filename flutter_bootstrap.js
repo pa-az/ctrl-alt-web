9e7dfa88d1d3d03cb318670d1631cec27fd44075
paazi <pozarpak@gmail.com>
2026-06-28 18:34:48 -0700

    Deploy webapp: fix syntax error in brain map layout

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "343661860" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2490789193" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
