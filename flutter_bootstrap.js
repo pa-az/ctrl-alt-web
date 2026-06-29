bb72eba395de21b9207b151f16ea212124e366d4
paazi <pozarpak@gmail.com>
2026-06-29 11:48:26 -0700

    Deploy webapp: Redesign matrix tactics menu to use category icon bubbles

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "683626256" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2220669871" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
