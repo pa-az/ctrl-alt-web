ec812ec44c2cf5b9bc75bb016aa9cc2122b846a3
paazi <pozarpak@gmail.com>
2026-06-28 21:39:55 -0700

    Deploy webapp: fix hit testing on brain map popups

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4020447755" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "301035327" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
