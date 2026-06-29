7bb86b09573d1e830d6fc500433e2bf62154e82c
paazi <pozarpak@gmail.com>
2026-06-29 13:14:33 -0700

    Deploy webapp: Fix PlatformsView build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2828616022" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "379580915" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
