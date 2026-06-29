070299d62ef70c77ccf8f09c51af9a36e168750d
paazi <pozarpak@gmail.com>
2026-06-29 16:51:40 -0700

    Deploy webapp: Make category icons 2 sizes smaller

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4026612613" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3287119109" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
