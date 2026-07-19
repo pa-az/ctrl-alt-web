d9d1684793dc0698d5268858e58cffadeccd20c4
paazi <pozarpak@gmail.com>
2026-07-19 05:46:02 -0700

    deploy: header filter button + support-page copy

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3084327644" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4287709789" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
